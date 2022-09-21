b98b96d4a08e131baa77abbe2638bfcf281b65b5
Henry Andrew Medina Condo <henry.medina@tecsup.edu.pe>
2022-09-21 10:01:22 +0200

    add more changes wednesday

--- Semana08/Day04/project01/js/app.js
+++ Semana08/Day04/project01/js/app.js
@@ -1,6 +1,11 @@
 import { getProducts,createProduct,deleteProduct } from "./productService.js";
 import { drawProducts } from "./interface.js";
 
+const btnCreate=document.getElementById("btnCreate");
+const modalCreate=document.getElementById("modalCreate")
+const bsModalCreate=new bootstrap.Modal(modalCreate)
+const formCreate=document.getElementById("formCreate")
+
 const obtainProducts=async()=>{
     try{
         const products=await getProducts();
@@ -8,29 +13,33 @@ const obtainProducts=async()=>{
     }catch(error){
         console.log("Ocurrio un error al obtener los datos.")
     }
-     
-    // 
-    // console.log(products)
-    
 }
 obtainProducts();
-// const draw=async()=>{
-    
-let np={
-    id:"18"
-}
 
-// const addProduct=async()=>{
-//     const npp=await createProduct(np)
-//     console.log(npp)
-// }
-
-const eliminateProduct=async()=>{
-    const rpta=await deleteProduct(np)
-    console.log(rpta)
-}
-// eliminateProduct()
-
-// addProduct()
-
-// }
+btnCreate.addEventListener("click",()=>{
+    bsModalCreate.show()
+})
+// Le indicamos que sea asincrono pq tiene q esperar la rpta del fetch
+formCreate.addEventListener("submit",async(event01)=>{
+    //prevent default <- para que no vuelva a recargar la pagina
+    event01.preventDefault();
+    // get values of input
+    const newProduct={
+        p_name:formCreate.p_name.value,
+        p_price:formCreate.p_price.value,
+        p_desc:formCreate.p_desc.value
+    }
+    try{
+        await createProduct(newProduct)
+        formCreate.reset()
+        bsModalCreate.hide()
+        obtainProducts()
+        Swal.fire({
+            icon:'success',
+            title:'Éxito',
+            text:'Successfull Created Product'
+        })
+    }catch(error){
+        alert("Ocurrio un error al crear el product.")
+    }
+})
