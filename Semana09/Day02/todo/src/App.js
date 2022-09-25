d73853508c8e8b3b56d3c05c5ba8107354f47e9b
Henry Andrew Medina Condo <henry.medina@tecsup.edu.pe>
2022-09-25 23:20:35 +0200

    add changesss

--- Semana09/Day02/todo/src/App.js
+++ Semana09/Day02/todo/src/App.js
@@ -1,22 +1,39 @@
-// import React from 'react'
-import {useEffect,useState} from "react"
+import {useState} from "react"
 
 export default function App() {
-  const [works,setWorks]=useState([45,23])
-  const [work,setWork]=useState("txtAndrew")
+  const [works,setWorks]=useState(["Work001","Work002"])
+  const [work,setWork]=useState("WorkToChange")
+  const addWork=()=>{
+    setWorks([...works,work])
+    setWork("")
+  }
+  const deleteWork=(indice)=>{
+    let worksTMP=[...works]
+    worksTMP.splice(indice,1)
+    setWorks(worksTMP)
+  }
   return (
     <div>
       <h1>App Andrew</h1>
       {works.map((item,index)=>(
-        <li key={index}>{item}</li>
+        <div key={index}>
+          <li >{item}</li>
+          <button onClick={()=>{deleteWork(index)}}>
+            Delete Work
+          </button>
+        </div>
+        
       ))}
-
       <input
         type="text"
         placeholder="Lavar la ropa"
         value={work}
         onChange={(e)=>{{setWork(e.target.value)}}}
         ></input>
+
+        <button onClick={()=>{addWork()}}>
+          Add Work
+        </button>
 
     </div>
   )
